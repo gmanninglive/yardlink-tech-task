3c6ba411beb7f1b83bf4a83dbeabfce46b36728a
gmanninglive <gmanning@live.co.uk>
2021-10-12 23:16:41 +0100

    Minor fix up to reviews section

--- components/reviews/Reviews.jsx
+++ components/reviews/Reviews.jsx
@@ -1,29 +1,40 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import reviews from './reviewdata.json';
+import reviews from "./reviewdata.json";
 
-export default function Reviews(){
-
-    return ( 
-        <>  
-        <section className="p-16 w-full bg-[#F8F8F5]">
+export default function Reviews() {
+  return (
+    <>
+      <section className="p-16 w-full bg-[#F8F8F5]">
+        {/* Title */}
         <h1 className="text-2xl font-bold">What Our Customers Are Saying...</h1>
-            <div className="md:py-16 flex justify-evenly flex-wrap ">
-                {reviews.map((review, idx) => {
-                    return ( 
-                        <div key={idx} className="w-1/4  bg-white p-6">
-                        <Image src={review.image} width={100} height={100} alt={review.name} />
-                        <p>{review.name}</p>
-                        <p>{review.business}</p>
-                        <p>{review.text}</p>
-                        </div>
-                    )
-                })}
 
-            </div>
-            </section>
-        </>
-    )
-
-
+        {/* Reviews */}
+        <div className="md:py-16 flex justify-evenly flex-wrap ">
+          {reviews.map((review, idx) => {
+            return (
+              <div key={idx} className="w-1/4  bg-white p-6 rounded-md shadow-md ">
+                {/* Photo, Name, Company Name */}
+                <div className="flex flex-wrap gap-x-6 mb-2">
+                  <Image
+                    src={review.image}
+                    width={100}
+                    height={100}
+                    alt={review.name}
+                    className="rounded-full"
+                  />
+                  <span className="w-1/2 text-center">
+                    <p className="font-bold">{review.name}</p>
+                    <p>{review.company}</p>
+                  </span>
+                </div>
+                {/* Testimonial Text */}
+                <p>{review.text}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
