e3383857f98c3c357a4ff0dca601fe765fa66d77
gmanninglive <gmanning@live.co.uk>
2021-10-14 14:06:18 +0100

    Dropdown Fleshed out

--- components/form/custominput/DropdownSelection.jsx
+++ components/form/custominput/DropdownSelection.jsx
@@ -5,9 +5,40 @@ import { Field } from "formik";
 
 export default function DropdownSelection({title, fieldName, data, className}){
 
-    console.log(title, fieldName, data, className);
+    const [isOpen, setIsOpen] = useState();
+
     return (
         <>
+        <div className="relative">
+        <button
+          className={`${className} inline-flex items-center`}
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {title}
+        </button>
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } w-full absolute top-15 bg-white z-50 divide-y grid`}
+        >
+          {data.map((option, idx) => (
+            <label
+              htmlFor={option}
+              className={`w-full p-2 relative z-8`}
+              key={idx}
+            >
+              {option}
+              <Field
+                type="checkbox"
+                value={option}
+                name={fieldName}
+                className="opacity-0 absolute left-0 bottom-0 h-full w-full cursor-pointer z-10"
+              />
+            </label>
+          ))}
+        </div>
+      </div>
         </>
     )
 }
