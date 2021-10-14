e659b074930037d09bb2735d21373f95ae13001b
gmanninglive <gmanning@live.co.uk>
2021-10-14 17:18:54 +0100

    Consume API featured product data

--- components/cards/StandardCard.jsx
+++ components/cards/StandardCard.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 StandardCard.propTypes = {
     data: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        thumbnail: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        image_url: PropTypes.string.isRequired,
         price: PropTypes.string //Not required.
     })
 }
@@ -12,18 +12,20 @@ StandardCard.propTypes = {
 // TODO Add link with animation
 
 export default function StandardCard({ data }) {
-  const { thumbnail, name, price } = data;
+  const { image_url,url, title, price } = data;
 
   return (
     <>
       <div className="w-2/12 flex flex-col flex-grow items-center ">
+          <a href={url}>
         <div className="flex justify-start gap-x-4">
-          <Image src={thumbnail} width={100} height={100} alt={name} />
+          <Image src={image_url} width={100} height={100} alt={title} />
 
           {/* If No price variable price is removed */}
           {!!price && <p className="text-sm leading-tight">{price}</p>}
         </div>
-        <p className="p-4 font-bold">{name}</p>
+        <p className="p-4 font-bold">{title}</p>
+        </a>
       </div>
     </>
   );
--- components/products/Featured.jsx
+++ components/products/Featured.jsx
@@ -3,7 +3,11 @@ import StandardCard from "../cards/StandardCard";
 
 import featuredProducts from "./featuredproducts.json";
 
-export default function Featured() {
+export default function Featured({data}) {
+
+  let products = data.concat(featuredProducts);
+
+  console.log(products)  
   return (
     <>
       <div className="p-12 ">
@@ -11,7 +15,7 @@ export default function Featured() {
         <h3 className="py-12 text-2xl font-bold">Featured Products</h3>
         {/* Products */}
         <div className="flex flex-wrap  gap-x-6">
-          {featuredProducts.map((product, idx) => {
+          {products.map((product, idx) => {
             return (
                 <StandardCard data={product} key={idx} />
             );
