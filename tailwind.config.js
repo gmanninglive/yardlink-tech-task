e9074deacc1ceb24e3d5071066904acb23000464
gmanninglive <gmanning@live.co.uk>
2021-10-14 13:30:36 +0100

    Add form dependencies and config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     extend: {
       colors:{
         orange: "#E9520F",
-        yardblue:"#102754"
+        yardblue:"#102754",
+        darkblue: "#243367",
+        lightgray: "#F7F9FD",
+        lightblue: "DDE4ED",
+        lightgreen: "F7FDF9",
+        darkgreen: "#6EA066",
+        lightred: "#FFE7E2",
+        darkred: "#A06670",
+        orange: "#E9520F",
       },
 
       translate:{
@@ -20,7 +28,7 @@ module.exports = {
 
       backgroundImage: {
         'hero': "url('/images/LandingBackground.jpg')",
-        
+        'select-icon': "url('/Polygon.svg')"
        }
     },
   },
