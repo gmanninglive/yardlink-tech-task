d1417d9d955789ce0bd3105e5a7f3576c3b621b1
gmanninglive <gmanning@live.co.uk>
2021-10-15 13:04:38 +0100

    Remove products concat

--- components/products/Featured.jsx
+++ components/products/Featured.jsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
 import StandardCard from "../cards/StandardCard";
 
 import featuredProducts from "./featuredproducts.json";
 
 export default function Featured({data}) {
 
-  let products = data.concat(featuredProducts);
-
-  console.log(products)  
   return (
     <>
       <div className="p-12 ">
@@ -15,7 +11,7 @@ export default function Featured({data}) {
         <h3 className="py-12 text-2xl font-bold">Featured Products</h3>
         {/* Products */}
         <div className="flex flex-wrap  gap-x-6">
-          {products.map((product, idx) => {
+          {data.map((product, idx) => {
             return (
                 <StandardCard data={product} key={idx} />
             );
